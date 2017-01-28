11497d4790e2a070541bea22a8c1368fd04f4ae2
Steve <steven.e11373@gmail.com>
2017-01-28 16:18:00 -0500

    adjusted

---
+++ frontend-nanodegree-Neighborhood Map-Project 6/js/app.js
@@ -0,0 +1,91 @@
+var locations = [{
+    title: 'The Coffee Shop',
+    location: {
+        lat: 40.7592781,
+        lng: -74.0033906
+    }
+}, {
+    title: 'Culture Espresso',
+    location: {
+        lat: 40.7520976,
+        lng: -73.9879236
+    }
+}, {
+    title: 'Hi-Collar',
+    location: {
+        lat: 40.7292516,
+        lng: -73.9882104
+    }
+}, {
+    title: 'Grounded Organic Coffee & Tea House',
+    location: {
+        lat: 40.7380567,
+        lng: -74.0056069
+    }
+}, {
+    title: 'Neil\'s Coffee Shop',
+    location: {
+        lat: 40.768984,
+        lng: -73.9652919
+    }
+}, {
+    title: 'Abraço',
+    location: {
+        lat: 40.7273576,
+        lng: -73.9883414
+    }
+}, {
+    title: 'Third Rail Coffee',
+    location: {
+        lat: 40.7273776,
+        lng: -73.9949075
+    }
+}, {
+    title: 'Andrew\'s Coffee Shop',
+    location: {
+        lat: 40.7517254,
+        lng: -73.9919929
+    }
+}, {
+    title: 'Ralphs Coffee',
+    location: {
+        lat: 40.7617722,
+        lng: -73.976553
+    }
+}, {
+    title: 'Ninth Street Espresso',
+    location: {
+        lat: 40.7270802,
+        lng: -73.991194
+    }
+}, {
+    title: 'El Rey',
+    location: {
+        lat: 40.7334803,
+        lng: -73.9929449
+    }
+}, {
+    title: 'Gregorys Coffee',
+    location: {
+        lat: 40.7539067,
+        lng: -73.9847976
+    }
+}, {
+    title: 'Everyman Espresso',
+    location: {
+        lat: 40.7420027,
+        lng: -73.9871945
+    }
+}, {
+    title: 'Ground Central Coffee Company',
+    location: {
+        lat: 40.7592014,
+        lng: -73.9876671
+    }
+}, {
+    title: 'Stumptown Coffee Roasters',
+    location: {
+        lat: 40.779667,
+        lng: -73.969
+    }
+}];
